c19aea795b3dfb9b8aa9dcfd44406cf9a774c25e
Ashwin Madavan <ashwin@Ashwins-MacBook-Pro.local>
2016-09-26 10:57:16 -0700

    Made Trie thread-safe

--- swara-learn/src/main/java/com/swara/ml/common/Trie.java
+++ swara-learn/src/main/java/com/swara/ml/common/Trie.java
@@ -2,7 +2,6 @@ package com.swara.ml.common;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.locks.StampedLock;
 import java.util.function.Function;
@@ -13,13 +12,13 @@ import lombok.EqualsAndHashCode;
  * A simple implementation of a generalized trie. A trie or prefix tree, is a data structure
  * optimized for prefix lookups. Unlike other trie implementations like the well-known PatriciaTrie,
  * this implementation makes no claim to be space-optimized; each node of the trie stores exactly
- * one key and value. Implementation features thread-safe get/put operations.
+ * one key and value. Implementation is thread-safe.
  *
  * @param <K> Key Type
  * @param <V> Value Type
  */
 @EqualsAndHashCode(exclude = { "parent", "children", "value" })
-public class Trie<K extends Comparable<K>, V> implements Comparable<Trie<K, V>>, Iterable<Trie<K, V>> {
+public class Trie<K extends Comparable<K>, V> implements Comparable<Trie<K, V>> {
 
     private final Trie<K, V> parent;
     private final List<Trie<K, V>> children;
@@ -65,6 +64,14 @@ public class Trie<K extends Comparable<K>, V> implements Comparable<Trie<K, V>>,
     }
 
     /**
+     * Returns the unmodifiable list of children of this trie node. Because the list is immutable,
+     * this method is thread safe and does not require any explicit locking.
+     */
+    public List<Trie<K, V>> children() {
+        return Collections.unmodifiableList(this.children);
+    }
+
+    /**
      * Returns the closest trie node to the specified sequence. Implementation is thread-safe, and
      * utilizes binary search at each level of the trie. Therefore, get operations can be performed
      * in O(k log n) time, where k is the length of the sequence and n is the average # of children.
@@ -73,9 +80,12 @@ public class Trie<K extends Comparable<K>, V> implements Comparable<Trie<K, V>>,
         if (seq == null || seq.isEmpty()) {
             return this;
         } else {
+            final long stamp = this.lock.readLock();
             final Trie<K, V> search = new Trie<>(this, seq.get(0), null);
             int index = Collections.binarySearch(this.children, search);
-            return index < 0 ? this : this.children.get(index).get(seq.subList(1, seq.size()));
+            final Trie<K, V> result = index < 0 ? null : this.children.get(index);
+            this.lock.unlock(stamp);
+            return result == null ? this : result.get(seq.subList(1, seq.size()));
         }
     }
 
@@ -90,17 +100,21 @@ public class Trie<K extends Comparable<K>, V> implements Comparable<Trie<K, V>>,
     public void put(List<K> seq, Function<V, V> update) {
         if (seq != null && !seq.isEmpty()) {
             // Find the child whose key matches the first element in the sequence.
+            long stamp = this.lock.readLock();
             final Trie<K, V> search = new Trie<>(this, seq.get(0), null);
             int index = Collections.binarySearch(this.children, search);
+            this.lock.unlock(stamp);
 
             // If no child exists then create a new one, making sure to preserve order.
             if (index < 0) {
+                stamp = this.lock.writeLock();
                 this.children.add(-(index + 1), search);
+                this.lock.unlock(stamp);
             }
 
             // Use the specified function to atomically update the value and recurse.
             final Trie<K, V> tree = index < 0 ? search : this.children.get(index);
-            final long stamp = tree.lock.writeLock();
+            stamp = tree.lock.writeLock();
             tree.value = update.apply(tree.value);
             tree.lock.unlock(stamp);
             tree.put(seq.subList(1, seq.size()), update);
@@ -108,7 +122,7 @@ public class Trie<K extends Comparable<K>, V> implements Comparable<Trie<K, V>>,
     }
 
     /**
-     * Recursively removes this node and all its children. Implementation is not thread-safe, but
+     * Recursively removes this node and all its children. Implementation is thread-safe, and
      * can be performed in O(h) where h is the height of the trie.
      */
     public void remove() {
@@ -116,17 +130,14 @@ public class Trie<K extends Comparable<K>, V> implements Comparable<Trie<K, V>>,
         this.children.forEach(Trie::remove);
 
         // Remove the tree from the parent.
+        final long stamp = this.parent.lock.writeLock();
         this.parent.children.remove(this);
+        this.parent.lock.unlock(stamp);
     }
 
     @Override
     public int compareTo(Trie<K, V> rhs) {
         return this.key.compareTo(rhs.key);
-    }
-
-    @Override
-    public Iterator<Trie<K, V>> iterator() {
-        return this.children.iterator();
     }
 
 }
