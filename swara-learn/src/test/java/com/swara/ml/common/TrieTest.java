99500c3ca581e6634413f903e7492dd5c225470f
Ashwin Madavan <ashwin@Ashwins-MacBook-Pro.local>
2016-09-27 17:24:44 -0700

    Added thread safety test

--- swara-learn/src/test/java/com/swara/ml/common/TrieTest.java
+++ swara-learn/src/test/java/com/swara/ml/common/TrieTest.java
@@ -2,6 +2,10 @@ package com.swara.ml.common;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,7 +13,7 @@ import org.junit.Test;
 public class TrieTest {
 
     @Test
-    public void testOperations() {
+    public void testGetAfterPutAndRemove() {
         final Trie<Character, Long> trie = new Trie<>();
         final List<Character> k1 = Arrays.asList('h', 'e', 'l', 'l', 'o');
         final List<Character> k2 = Arrays.asList('h', 'e', 'l', 'p');
@@ -31,6 +35,27 @@ public class TrieTest {
         Assert.assertNull(trie.get(k1));
         Assert.assertNull(trie.get(k2));
         Assert.assertNotNull(trie.get(Arrays.asList('h', 'e')));
+    }
+    
+    @Test
+    public void testPutThreadSafety() throws InterruptedException {
+        final Trie<Character, Long> trie = new Trie<>();
+        final List<Character> key = Arrays.asList('h', 'e', 'l', 'l', 'o');
+        final ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        // Submit simulatenous put operations.
+        IntStream.range(0, 10).forEach(i -> executor.submit(
+            () -> trie.put(key, v -> v == null ? 0L : ++v))
+        );
+
+        // Await the completion of the put operations.
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
+
+        // Verify that the modifications were applied atomically.
+        IntStream.range(1, key.size()).forEach(i ->
+            Assert.assertEquals(Long.valueOf(9L), trie.get(key.subList(0, i)).value())
+        );
     }
 
 }
