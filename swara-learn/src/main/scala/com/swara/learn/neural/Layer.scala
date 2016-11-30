159eecc2891d33ab51bdb05f788ab71ea21b8249
Ashwin Madavan <ashwin.madavan@gmail.com>
2016-11-30 16:31:58 +0000

    Update Layer.scala

--- swara-learn/src/main/scala/com/swara/learn/neural/Layer.scala
+++ swara-learn/src/main/scala/com/swara/learn/neural/Layer.scala
@@ -4,8 +4,12 @@ import breeze.linalg.Tensor
 
 trait Layer[I, O] {
 
-  def forward(input: I): O
-
-  def backward(seq: Seq[(I, O, O)]): Seq[I]
+  /**
+   * Applies the layer to the specified sequence of inputs. Returns a tuple containing the 
+   * outputs of the layer as well as a backpropagation function that takes a sequence of
+   * output errors, calculates and applies weight updates, and returns a sequence of input 
+   * errors.
+   */
+  def apply(x: Seq[I]): (Seq[O], Seq[O] => Seq[I])
 
 }
